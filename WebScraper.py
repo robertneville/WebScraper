fa66ce517778d3d8b025f9891812977a1114c593
robertneville <ran4@students.waikato.ac.nz>
2018-04-09 23:53:26 +1200

    added some code to get Facebook website. I can find the website but I cannot extract anything yet.

--- WebScraper.py
+++ WebScraper.py
@@ -1,3 +1,8 @@
+# import libraries
+import urllib3
+import bs4
+import certifi
+
 
 class WebScraper:
     def __init__(self):
@@ -16,6 +21,22 @@ class WebScraper:
 def test(my_web_scraper):
     print("Your username is: " + my_web_scraper.username + "\n")
     print("Your password is: " + my_web_scraper.password + "\n")
+    # the url I am using
+    quote_page = 'https://www.facebook.com'
+    # create a PoolManager to make requests
+    page = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
+    # make a request to a web page
+    request = page.request('GET', quote_page)
+    soup = bs4.BeautifulSoup(request, 'html.parser')
+    # Take out the <div> of name and get its value
+    name_box = soup.find('input', attrs={'class': 'inputtext', 'name': 'email'})
+    # strip() is used to remove starting and trailing
+    name = name_box
+    print(name)
+    # get the index price
+    price_box = soup.find('div', attrs={'class': 'price’'})
+    price: str = price_box
+    print(price)
 
 
 if __name__ == "__main__":
